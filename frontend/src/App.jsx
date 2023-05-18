a85d9bfc9c1cd3b0d708461d66d273ba3211dc04
Nosa Agho <nosaoasis@gmail.com>
2023-05-18 23:27:46 +0100

    Navigation Bar
    Implemented the design requirement and feature for the navigation bar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,18 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
-import TopicListItem from "./components/TopicListItem";
 import PhotoList from "./components/PhotoList";
-import TopicList from "./components/TopicList";
+import TopNavigationBar from "./components/TopNavigationBar";
 import "./App.scss";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photoArray = [...Array(3)].map((photo) => (
-  //   <PhotoListItem key={new Date().toLocaleString()} />
-  // ));
-
-  // const topicArray = [...Array(3)].map((topic) => (
-  //   <TopicListItem key={new Date().toLocaleString()} />
-  // ));
-
   return (
-    <div className="App">
-      {/* {topicArray} */}
-      <TopicList />
-      <PhotoList />
-    </div>
+    <>
+      <div className="App">
+      <TopNavigationBar />
+        <PhotoList />
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/TopNavigationBar.scss'
+import "../styles/TopNavigationBar.scss";
+import TopicList from "./TopicList";
+import FavBadge from "./FavBadge";
 
 const TopNavigation = () => {
   return (
-    <div className="top-nav-bar">
-      <span className="top-nav-bar--logo">PhotoLabs</span>
-    </div>
-  )
-}
+    <>
+      <div className="top-nav-bar">
+        <span className="top-nav-bar--logo">PhotoLabs</span>
+        <TopicList />
+        <FavBadge />
+      </div>
+    </>
+  );
+};
 
 export default TopNavigation;
